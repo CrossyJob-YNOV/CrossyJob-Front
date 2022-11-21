b88312c8b841481c9c1d1dc6e863bd1077af8c7f
Tristan264 <tristan.bschwenk@icloud.com>
2022-11-21 17:16:49 +0100

    feat(app): create homepage layout

---
+++ app/components/Nav.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="nav">
+        <h1>CrossyJob</h1>
+
+        <div class="nav__content">
+            <h3>Dernieres offres</h3>
+            <h3>Classements</h3>
+            <h3>Accueil</h3>
+        </div>
+
+        <div class="nav__profile">
+            Profile
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .nav {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        background: white;
+
+        .nav__content {
+            display: flex;
+            justify-content: center;
+            column-gap: 1em;
+        }
+
+        .nav__profile {
+            align-self: center;
+            justify-self: end;
+        }
+    }
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="homepage">
+    <div class="homepage__reviews">
+      <div class="homepage__reviews__filter">
+        <h3>Best Matches</h3>
+        <h3>Featured</h3>
+        <h3>Most Recent</h3>
+      </div>
+
+      <div class="homepage__reviews__list">
+        <div class="homepage__reviews__list__item">
+
+        </div>
+      </div>
+    </div>
+
+    <div class="homepage__review">
+
+    </div>
+
+    <div class="homepage__personnal-information">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout (context) {
+    return "home"
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .homepage {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 2em;
+  }
+</style>
