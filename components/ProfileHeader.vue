b6a7f04730322b5e86a1f1043fa108328c541147
Tristan264 <tristan.bschwenk@icloud.com>
2023-05-14 23:59:59 +0200

    feat(profile): create ProfileHeader component

---
+++ components/ProfileHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="profile-content__header mb-5">
+    <div class="profile-content__grid">
+      <img src="/images/user-picto.png" alt="" />
+      <div>
+        <h2>{{user.firstname}} {{ user.lastname }}</h2>
+      </div>
+    </div>
+    <button>Modifier le profil</button>
+  </div>
+</template>
+
+<script >
+export default {
+  name: 'dashboard',
+  layout(context) {
+    return 'profile'
+  },  
+  data() {
+    return {
+      user: {}
+    }
+  },
+
+  mounted() {
+    this.user = this.$auth.user
+  },  
+}
+
+</script>
+
+<style lang="scss">
+.profile-content {
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    img {
+      width: 80px;
+    }
+  }
+}
+</style>
